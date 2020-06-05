ba5e72c6f2b86f80084b73ef2b13e7ebaa145915
Votre nom ou pseudo <remy.pouzet@laposte.net>
2020-06-05 17:50:22 +0200

    -fix somes bugs TODO : manage view binding and implement login screens and login behavior

--- app/src/main/java/remy/pouzet/go4lunch/BaseActivity.java
+++ app/src/main/java/remy/pouzet/go4lunch/BaseActivity.java
@@ -1,10 +1,13 @@
 package remy.pouzet.go4lunch;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import remy.pouzet.go4lunch.databinding.ActivityLoginBinding;
 
 /**
  * Created by Philippe on 12/01/2018.
@@ -16,10 +19,15 @@ public abstract class BaseActivity extends AppCompatActivity {
     // LIFE CYCLE
     // --------------------
     
+    private ActivityLoginBinding binding;
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(this.getFragmentLayout());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(this.getFragmentLayout());
+        
     }
     
     public abstract int getFragmentLayout();
--- app/src/main/java/remy/pouzet/go4lunch/ui/login/LoginActivity.java
+++ app/src/main/java/remy/pouzet/go4lunch/ui/login/LoginActivity.java
@@ -1,5 +1,9 @@
 package remy.pouzet.go4lunch.ui.login;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 import remy.pouzet.go4lunch.BaseActivity;
 import remy.pouzet.go4lunch.R;
 
@@ -8,6 +12,36 @@ public class LoginActivity extends BaseActivity {
 	@Override
 	public int getFragmentLayout() {
 		return R.layout.activity_login;
+	}
+	
+	//FOR DATA
+	// 1 - Identifier for Sign-In Activity
+	private static final int RC_SIGN_IN = 123;
+	
+	// --------------------
+	// ACTIONS
+	// --------------------
+	
+	//@OnClick(R.id.main_activity_button_login)
+	public void onClickLoginButton() {
+		// 3 - Launch Sign-In Activity when user clicked on Login Button
+		this.startSignInActivity();
+	}
+	
+	// --------------------
+	// NAVIGATION
+	// --------------------
+	
+	// 2 - Launch Sign-In Activity
+	private void startSignInActivity() {
+		startActivityForResult(AuthUI
+				                       .getInstance()
+				                       .createSignInIntentBuilder()
+				                       .setTheme(R.style.LoginTheme)
+				                       .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+				                       .setIsSmartLockEnabled(false, true)
+				                       .setLogo(R.drawable.ic_logo_auth)
+				                       .build(), RC_SIGN_IN);
 	}
 	
 }
