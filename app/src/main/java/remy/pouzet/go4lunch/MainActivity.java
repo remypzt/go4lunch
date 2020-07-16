309a2c834b274e89f36e4aefa4347876a371e535
Votre nom ou pseudo <remy.pouzet@laposte.net>
2020-07-16 13:57:47 +0200

    -

--- app/src/main/java/remy/pouzet/go4lunch/MainActivity.java
+++ app/src/main/java/remy/pouzet/go4lunch/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
 	//------------------------------------------------------//
 	// 0------------------   Binding    ------------------- //
 	//------------------------------------------------------//
-	
 	//------------------------------------------------------//
 	// ------------------   Variables   ------------------- //
 	//------------------------------------------------------//
@@ -210,5 +209,42 @@ public class MainActivity extends AppCompatActivity {
 			}
 		};
 	}
-	
+//
+//	public void autoCompleteSearch() {
+//		// Create a new token for the autocomplete session. Pass this to FindAutocompletePredictionsRequest,
+//		// and once again when the user makes a selection (for example when calling fetchPlace()).
+//		AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
+//
+//		// Create a RectangularBounds object.
+//		RectangularBounds bounds = RectangularBounds.newInstance(new LatLng(-33.880490, 151.184363), new LatLng(-33.858754, 151.229596));
+//		// Use the builder to create a FindAutocompletePredictionsRequest.
+//		FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
+//		                                                                               // Call either setLocationBias() OR setLocationRestriction().
+////		                                                                               .setLocationBias(bounds)
+////		                                                                               //.setLocationRestriction(bounds)
+////		                                                                               .setOrigin(new LatLng(-33.8749937, 151.2041382))
+////		                                                                               .setCountries("AU", "NZ")
+////		                                                                               .setTypeFilter(TypeFilter.ADDRESS)
+////		                                                                               .setSessionToken(token)
+//		                                                                               .setQuery(query)
+//		                                                                               .build();
+//
+//		placesClient
+//				.findAutocompletePredictions(request)
+//				.addOnSuccessListener((response) -> {
+//					for (AutocompletePrediction prediction : response.getAutocompletePredictions()) {
+//						Log.i(TAG, prediction.getPlaceId());
+//						Log.i(TAG, prediction
+//								.getPrimaryText(null)
+//								.toString());
+//					}
+//				})
+//				.addOnFailureListener((exception) -> {
+//					if (exception instanceof ApiException) {
+//						ApiException apiException = (ApiException) exception;
+//						Log.e(TAG, "Place not found: " + apiException.getStatusCode());
+//					}
+//				});
+//	}
+//
 }
