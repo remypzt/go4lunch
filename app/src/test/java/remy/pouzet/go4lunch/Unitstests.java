4a514dba32657ae62160f7690731e8706a06f021
Votre nom ou pseudo <remy.pouzet@laposte.net>
2020-09-12 21:12:32 +0200

    - implements Unitstests

--- app/src/test/java/remy/pouzet/go4lunch/Unitstests.java
+++ app/src/test/java/remy/pouzet/go4lunch/Unitstests.java
@@ -41,8 +41,7 @@ public class Unitstests {
 		RestaurantsRepository localRestaurantsRepository = new RestaurantsRepository();
 
 //		TestCase.assertTrue(localRestaurantsRepository.openingStatus());
-//
-//
+	
 	}
 	
 	@Test
